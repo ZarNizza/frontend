e932fc0f0fbcbd4c618a3a3999770d8c636fce60
ZarNizza <gormaster@mail.ru>
2021-12-05 21:54:31 +0400

    log UserID's to console

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 // curl -X POST -v localhost:8002/users   status 201
 // curl -X POST -v localhost:8002/users123      bad request = err404
 // curl -v localhost:8002   list of Users, status 200
+// curl --data '{"name":"xyz","password":"xyz"}' -v localhost:8000/users
